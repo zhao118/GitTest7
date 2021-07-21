01379f048a109efdd98e985830e1d124f77e7b76
赵强 <568423952@qq.com>
2021-07-21 16:00:01 +0800

    增加了打印

--- GitTest7/ViewController.swift
+++ GitTest7/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     print("nihao")
+        print("nihao")
+        print("abcdefg")
     }
-
-
+    
+    
 }
 
